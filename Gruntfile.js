c170e267334a3b6ad81189b611fc27a6684d733f
Andy Dai <daikeren@gmail.com>
2014-05-06 12:02:56 +0800

    Added grunt task for build and publish gitbook page.

---
+++ Gruntfile.js
@@ -0,0 +1,32 @@
+var path = require("path");
+
+module.exports = function (grunt) {
+    grunt.loadNpmTasks('grunt-gitbook');
+    grunt.loadNpmTasks('grunt-gh-pages');
+    grunt.loadNpmTasks('grunt-contrib-clean');
+
+    grunt.initConfig({
+        'gitbook': {
+            development: {
+                input: "./",
+                github: "daikeren/django_tutorial"
+            }
+        },
+        'gh-pages': {
+            options: {
+                base: '_book'
+            },
+            src: ['**']
+        },
+        'clean': {
+            files: '.grunt'
+        }
+    });
+
+    grunt.registerTask('publish', [
+        'gitbook',
+        'gh-pages',
+        'clean'
+    ]);
+    grunt.registerTask('default', 'gitbook');
+};
